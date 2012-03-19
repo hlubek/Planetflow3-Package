3caf775309b26bdf9596f490197591ca218cec52
Christopher Hlubek <hlubek@networkteam.com>
2012-03-19 12:57:09 +0100

    [TASK] Fix loader, some mobile enhancements

--- Resources/Public/Media/JavaScripts/script.js
+++ Resources/Public/Media/JavaScripts/script.js
@@ -1,7 +1,9 @@
 $('#main').infinitescroll({
-	navSelector  : ".pagebrowser",
-	nextSelector : ".pagebrowser a.next",
-	itemSelector : "#main article.item"
+	navSelector: ".pagebrowser",
+	nextSelector: ".pagebrowser a.next",
+	itemSelector: "#main article.item",
+	loadingImg: "/_Resources/Static/Packages/Planetflow3/Media/Images/ajax-loader.gif"
+
 });
 $('#metamenu .imprint').click(function() {
 	$('#imprint').toggle('fast');
